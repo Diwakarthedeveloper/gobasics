301039aab9cb646e11808d82c4531c4e46a2e568
Diwakar <djha4635@gmail.com>
2024-03-19 01:19:52 +0000

    Basics of Go

---
+++ Constants.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const earthsGravity = 9.80665 // Name of Constant should be in camelCase or PascalCase
+
+	fmt.Println("gravity of earth is ", earthsGravity)
+}
---
+++ Strings.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var snack string
+	var favoriteSnack string
+
+	favoriteSnack = "YOU"
+	//favoriteSnack = "My favorite snack is " + snack + "."
+
+	fmt.Println("My favorite snack is " + favoriteSnack)
+}
