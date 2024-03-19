1ccf21c710487de90186a522c3180554e269a877
Diwakar <djha4635@gmail.com>
2024-03-19 01:17:10 +0000

    way to assign variables in go

---
+++ AssignVariables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numOfFlavors int
+
+	numOfFlavors = 57
+
+	fmt.Println(numOfFlavors)
+
+	var flavorScale float32 = 5.8 // variable declaration and assign type  and value can be done in same line is value is know
+	fmt.Println(flavorScale)
+
+}
