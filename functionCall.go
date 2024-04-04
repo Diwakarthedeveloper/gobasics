6fb21d1a2a34b253a1a29e90588adea20394c18b
Diwakar <djha4635@gmail.com>
2024-04-04 19:49:06 +0000

    how to call a function in GO

---
+++ functionCall.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Defining a function named  eatTacos below:
+func eatTacos() {
+	fmt.Println("Yum!")
+}
+
+func main() {
+	// Calling my function below:
+	eatTacos()
+}
